51034ce3ca117bf1385dbe33f569796d049c3c55
Ryan-Thrall <ryanthrall04@gmail.com>
2023-06-13 11:52:10 -0600

    Setup Route to Game Page

--- Frontend.client/src/components/Navbar.vue
+++ Frontend.client/src/components/Navbar.vue
@@ -12,6 +12,11 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
+          <router-link :to="{ name: 'Game' }" class="btn text-success lighten-30 selectable text-uppercase">
+            Play
+          </router-link>
+        </li>
+        <li>
           <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
             About
           </router-link>
--- Frontend.client/src/router.js
+++ Frontend.client/src/router.js
@@ -21,6 +21,11 @@ const routes = [
     name: 'Account',
     component: loadPage('AccountPage'),
     beforeEnter: authGuard
+  },
+  {
+    path: '/game',
+    name: 'Game',
+    component: loadPage('GamePage'),
   }
 ]
 
